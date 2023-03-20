8e908ec629b0781e184359d5049e28af8c35fa03
shungo0222 <kimura.shungo@gmail.com>
2023-03-20 10:24:18 +0900

    CREATE DNS CONTRACT / Set up domains, resolvers and records / Test other users

--- scripts/run.js
+++ scripts/run.js
@@ -1,4 +1,5 @@
 const main = async () => {
+    // The first return is the deployer, the second is a random account
     const [owner, randomPerson] = await hre.ethers.getSigners();
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
     const domainContract = await domainContractFactory.deploy();
@@ -6,13 +7,26 @@ const main = async () => {
     console.log("Contract deployed to:", domainContract.address);
     console.log("Contract deployed by:", owner.address);
 
-    const txn = await domainContract.register("doom");
+    let txn = await domainContract.register("doom");
     await txn.wait();
 
-    const domainOwner = await domainContract.getAddress("doom");
-    console.log("Owner of domain: ", domainOwner);
+    const domainAddress = await domainContract.getAddress("doom");
+    console.log("Owner of domain: ", domainAddress);
+
+    // Trying to set a record that doesn't belong to me! => "Error: Transaction reverted without a reason string"
+    // txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
+    // await txn.wait();
+
+    console.log("======================================================================")
+
+    txn = await domainContract.connect(randomPerson).register("pochi");
+    await txn.wait();
+
+    console.log("Random person's address: ", randomPerson.address);
+    const randomAddress = await domainContract.getAddress("pochi");
+    console.log("Owner of pochi domain: ", randomAddress);
 };
-    
+
 const runMain = async () => {
     try {
         await main();
