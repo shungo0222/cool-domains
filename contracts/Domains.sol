a370476247c64ac8f0fe1357b665570cbd453ee1
shungo0222 <kimura.shungo@gmail.com>
2023-03-22 14:14:41 +0900

    CREATE DNS CONTRACT / Sell our domains as NFTs / Getting paid and going .ninja

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -2,23 +2,48 @@
 
 pragma solidity ^0.8.10;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
 import "hardhat/console.sol";
 
 contract Domains {
+    // My TLD
+    string public tld;
+
     // A "mapping" data type to store their names
     mapping(string => address) public domains;
 
     // Checkout our new mapping! This will store values
     mapping(string => string) public records;
 
-    constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+    // We make the contract "payable" by adding this to the constructor
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("%s name service deployed", _tld);
+    }
+
+    // This function will give us the price of a domain based on length
+    function price(string calldata name) public pure returns (uint) {
+        uint len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+            return 5 * 10**17;
+        } else if (len == 4) {
+            return 3 * 10**17;
+        } else {
+            return 1 * 10**17;
+        }
     }
 
     // A register function that adds their names to our mapping
-    function register(string calldata name) public {
+    function register(string calldata name) public payable {
         // Check that the name is unregistered
         require(domains[name] == address(0));
+
+        uint _price = price(name);
+
+        // Check if enough MATIC was paid in the transaction
+        require(msg.value >= _price, "Not enough MATIC paid");
+
         domains[name] = msg.sender;
         console.log("%s has registered a domain!", msg.sender);
     }
--- scripts/run.js
+++ scripts/run.js
@@ -1,30 +1,37 @@
 const main = async () => {
     // The first return is the deployer, the second is a random account
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    const domainContract = await domainContractFactory.deploy();
-    await domainContract.deployed();
-    console.log("Contract deployed to:", domainContract.address);
-    console.log("Contract deployed by:", owner.address);
+    // const [owner, randomPerson] = await hre.ethers.getSigners();
 
-    let txn = await domainContract.register("doom");
+    const domainContractFactory = await hre.ethers.getContractFactory('Domains');
+    // We pass in "udon" to the constructor when deploying
+    const domainContract = await domainContractFactory.deploy("udon");
+    await domainContract.deployed();
+
+    console.log("Contract deployed to:", domainContract.address);
+    // console.log("Contract deployed by:", owner.address);
+
+    // We're passing in a second variable - value. This is the money.
+    let txn = await domainContract.register("pochi", {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
 
-    const domainAddress = await domainContract.getAddress("doom");
-    console.log("Owner of domain: ", domainAddress);
+    const address = await domainContract.getAddress("pochi");
+    console.log("Owner of domain \"pochi\": ", address);
+
+    const balance = await hre.ethers.provider.getBalance(domainContract.address);
+    console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
 
     // Trying to set a record that doesn't belong to me! => "Error: Transaction reverted without a reason string"
     // txn = await domainContract.connect(randomPerson).setRecord("doom", "Haha my domain now!");
     // await txn.wait();
 
-    console.log("======================================================================")
+    // console.log("======================================================================")
 
-    txn = await domainContract.connect(randomPerson).register("pochi");
-    await txn.wait();
+    // txn = await domainContract.connect(randomPerson).register("pochi");
+    // await txn.wait();
 
-    console.log("Random person's address: ", randomPerson.address);
-    const randomAddress = await domainContract.getAddress("pochi");
-    console.log("Owner of pochi domain: ", randomAddress);
+    // console.log("Random person's address: ", randomPerson.address);
+    // const randomAddress = await domainContract.getAddress("pochi");
+    // console.log("Owner of pochi domain: ", randomAddress);
 };
 
 const runMain = async () => {
